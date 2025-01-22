3376d4c2cbbe3545d49ef80e79df934ecb5068f7
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-01-22 11:00:00 +0100

    feat(menu): add add-to-cart button and logic

--- src/app/features/menu/pages/menu-page.component.ts
+++ src/app/features/menu/pages/menu-page.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable ,Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { MenuItem } from '../../../core/services/menu-api.service';
 import * as MenuSelectors from '../store/menu.selectors'
 import * as MenuActions from '../store/menu.actions'
+import { orderActions } from '../../order/store/order.actions';
 import { CommonModule } from '@angular/common';
-import {AppState } from '../store/menu.state';
+import { AppState } from '../store/menu.state';
 import { take } from 'rxjs/operators';
 
 
 @Component({
   selector: 'app-menu-page',
   standalone: true,
-  templateUrl:'./menu-page.component.html',
-  styleUrl:'./menu-page.component.css',
- imports: [CommonModule],
+  templateUrl: './menu-page.component.html',
+  styleUrl: './menu-page.component.css',
+  imports: [CommonModule],
 })
-export class MenuPageComponent implements OnInit ,OnDestroy{
+export class MenuPageComponent implements OnInit, OnDestroy {
 
- menuItems$: Observable<MenuItem[]>;
+  menuItems$: Observable<MenuItem[]>;
   loading$: Observable<boolean>;
   error$: Observable<string | null>;
   showOnlyAvailable$: Observable<boolean>;
@@ -27,16 +28,16 @@ export class MenuPageComponent implements OnInit ,OnDestroy{
 
   private subscriptions: Subscription = new Subscription();
   MenuSelectors: any;
- currentPage$: Observable<number> | undefined;
+  currentPage$: Observable<number> | undefined;
   constructor(private store: Store<AppState>) {
-   // this.menuItems$ = this.store.select(MenuSelectors.selectFilteredMenuItems);
+    // this.menuItems$ = this.store.select(MenuSelectors.selectFilteredMenuItems);
     this.loading$ = this.store.select(MenuSelectors.selectMenuLoading);
     this.error$ = this.store.select(MenuSelectors.selectMenuError);
     this.showOnlyAvailable$ = this.store.select(MenuSelectors.selectShowOnlyAvailable);
     this.availableCount$ = this.store.select(MenuSelectors.selectAvailableCount);
     this.totalCount$ = this.store.select(MenuSelectors.selectTotalCount);
     this.menuItems$ = this.store.select(MenuSelectors.selectPaginatedMenuItems);
-     this.currentPage$ = this.store.select(MenuSelectors.selectCurrentPage); 
+    this.currentPage$ = this.store.select(MenuSelectors.selectCurrentPage);
   }
 
   ngOnInit(): void {
@@ -59,31 +60,42 @@ export class MenuPageComponent implements OnInit ,OnDestroy{
     this.store.dispatch(MenuActions.loadMenuItems());
   }
 
+  addToCart(item: MenuItem): void {
+    if (!item.disponible || !item.id) return;
+
+    this.store.dispatch(orderActions.addItem({
+      platId: item.id,
+      quantity: 1,
+      price: item.price ?? 0,
+      name: item.name
+    }));
+  }
+
   // Méthodes pour naviguer entre les pages
-goToPage(page: number) {
-  this.store.dispatch(MenuActions.changePage({ page }));
-}
-nextPage() {
-  this.currentPage$!
-    .pipe(take(1))
-    .subscribe(page => {
-      this.store.dispatch(
-        MenuActions.changePage({ page: page + 1 })
-      );
-    });
-}
-
-prevPage() {
-  this.currentPage$!
-    .pipe(take(1))
-    .subscribe(page => {
-      if (page > 1) {
+  goToPage(page: number) {
+    this.store.dispatch(MenuActions.changePage({ page }));
+  }
+  nextPage() {
+    this.currentPage$!
+      .pipe(take(1))
+      .subscribe(page => {
         this.store.dispatch(
-          MenuActions.changePage({ page: page - 1 })
+          MenuActions.changePage({ page: page + 1 })
         );
-      }
-    });
-}
+      });
+  }
 
-  
+  prevPage() {
+    this.currentPage$!
+      .pipe(take(1))
+      .subscribe(page => {
+        if (page > 1) {
+          this.store.dispatch(
+            MenuActions.changePage({ page: page - 1 })
+          );
+        }
+      });
+  }
+
+
 }
