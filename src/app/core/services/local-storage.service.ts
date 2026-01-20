ae2888a669c207d89a0e01eb0a97bc13798717bb
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-20 10:25:12 +0100

    implement set and remove methods in the LocalStorageService

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -8,5 +8,11 @@ export class LocalStorageService<T> {
     return data ? JSON.parse(data) as T : null;
   }
 
- 
+  set(key: string, value: T): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  remove(key: string): void {
+    localStorage.removeItem(key);
+  }
 }
