3380889e6ab5f551a3f85ccc55a43bfd332ab179
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-20 11:53:20 +0100

    create the architecture oder component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layout/main-layout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'menu',
+        loadChildren: () =>
+          import('./features/menu/menu.routes').then(m => m.MENU_ROUTES)
+      },
+      {
+        path: 'order',
+        loadChildren: () =>
+          import('./features/order/order.routes').then(m => m.ORDER_ROUTES)
+      },
+      {
+        path: 'complaint',
+        loadChildren: () =>
+          import('./features/complaint/complaint.routes').then(m => m.COMPLAINT_ROUTES)
+      }
+    ]
+  }
+];
