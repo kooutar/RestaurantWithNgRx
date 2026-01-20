955136380ae29b40a20e025cc0fd082170e41861
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-20 10:03:47 +0100

    install @ngrx/effects  librarie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideStore()
+    provideStore(),
+    provideEffects()
 ]
 };
