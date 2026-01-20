9563ff5f3158519919b8b09bf82eb8be190b11f6
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2026-01-20 15:52:57 +0100

    Update main layout component

--- src/app/core/layout/main-layout.component.ts
+++ src/app/core/layout/main-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-main-layout',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
     <header>Restaurant App</header>
     <router-outlet />
     <footer>© 2026</footer>
-  `
+  `,
+  imports: [RouterOutlet]
 })
 export class MainLayoutComponent {}
