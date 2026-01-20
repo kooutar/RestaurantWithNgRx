ed83822e5c75573d3ef17e0c0accd361e7f17c88
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-20 10:58:04 +0100

    create menu-page component

---
+++ src/app/features/menu/pages/menu-page/menu-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-page',
+  imports: [],
+  templateUrl: './menu-page.html',
+  styleUrl: './menu-page.css',
+})
+export class MenuPage {
+
+}
