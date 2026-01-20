e0b48873da8429d49dfa0d3ce81dbdff38b18ab9
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-20 10:24:33 +0100

    implemente get methode in localStorageService

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -3,5 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService<T> {
 
+  get(key: string): T | null {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) as T : null;
+  }
+
  
 }
