1c6dd37a43025f275e3557acec8c7e0b6c7e50f9
Achraf Sikal <achrafsikal@gmail.com>
2026-01-21 22:56:32 +0100

    feat: redesign menu page layout with enhanced item display, improved filtering options, and updated loading state; integrate order functionality for better user interaction

--- src/app/features/menu/pages/menu-page.component.ts
+++ src/app/features/menu/pages/menu-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable ,Subscription } from 'rxjs';
 import { MenuItem } from '../../../core/services/menu-api.service';
@@ -7,18 +7,20 @@ import * as MenuActions from '../store/menu.actions'
 import { CommonModule } from '@angular/common';
 import {AppState } from '../store/menu.state';
 import { take } from 'rxjs/operators';
+import { OrderState } from '../../order/store/order.state';
+import { orderActions } from '../../order/store/order.actions';
+import { Plat } from '../../order/order';
 
 
 @Component({
   selector: 'app-menu-page',
   standalone: true,
-  templateUrl:'./menu-page.component.html',
-  styleUrl:'./menu-page.component.css',
- imports: [CommonModule],
+  templateUrl: './menu-page.component.html',
+  styleUrl: './menu-page.component.css',
+  imports: [CommonModule],
 })
-export class MenuPageComponent implements OnInit ,OnDestroy{
-
- menuItems$: Observable<MenuItem[]>;
+export class MenuPageComponent implements OnInit, OnDestroy {
+  menuItems$: Observable<MenuItem[]>;
   loading$: Observable<boolean>;
   error$: Observable<string | null>;
   showOnlyAvailable$: Observable<boolean>;
@@ -27,21 +29,20 @@ export class MenuPageComponent implements OnInit ,OnDestroy{
 
   private subscriptions: Subscription = new Subscription();
   MenuSelectors: any;
- currentPage$: Observable<number> | undefined;
+  currentPage$: Observable<number> | undefined;
   constructor(private store: Store<AppState>) {
-   // this.menuItems$ = this.store.select(MenuSelectors.selectFilteredMenuItems);
+    // this.menuItems$ = this.store.select(MenuSelectors.selectFilteredMenuItems);
     this.loading$ = this.store.select(MenuSelectors.selectMenuLoading);
     this.error$ = this.store.select(MenuSelectors.selectMenuError);
     this.showOnlyAvailable$ = this.store.select(MenuSelectors.selectShowOnlyAvailable);
     this.availableCount$ = this.store.select(MenuSelectors.selectAvailableCount);
     this.totalCount$ = this.store.select(MenuSelectors.selectTotalCount);
     this.menuItems$ = this.store.select(MenuSelectors.selectPaginatedMenuItems);
-     this.currentPage$ = this.store.select(MenuSelectors.selectCurrentPage); 
+    this.currentPage$ = this.store.select(MenuSelectors.selectCurrentPage);
   }
 
   ngOnInit(): void {
-    this.store.dispatch(MenuActions.loadMenuItems())
-
+    this.store.dispatch(MenuActions.loadMenuItems());
   }
 
   ngOnDestroy(): void {
@@ -49,9 +50,7 @@ export class MenuPageComponent implements OnInit ,OnDestroy{
   }
 
   toggleFilter(showOnlyAvailable: boolean): void {
-    this.store.dispatch(
-      MenuActions.filterAvailableItems({ showOnlyAvailable })
-    );
+    this.store.dispatch(MenuActions.filterAvailableItems({ showOnlyAvailable }));
   }
 
   reload(): void {
@@ -60,30 +59,26 @@ export class MenuPageComponent implements OnInit ,OnDestroy{
   }
 
   // Méthodes pour naviguer entre les pages
-goToPage(page: number) {
-  this.store.dispatch(MenuActions.changePage({ page }));
-}
-nextPage() {
-  this.currentPage$!
-    .pipe(take(1))
-    .subscribe(page => {
-      this.store.dispatch(
-        MenuActions.changePage({ page: page + 1 })
-      );
+  goToPage(page: number) {
+    this.store.dispatch(MenuActions.changePage({ page }));
+  }
+  nextPage() {
+    this.currentPage$!.pipe(take(1)).subscribe((page) => {
+      this.store.dispatch(MenuActions.changePage({ page: page + 1 }));
     });
-}
+  }
 
-prevPage() {
-  this.currentPage$!
-    .pipe(take(1))
-    .subscribe(page => {
+  prevPage() {
+    this.currentPage$!.pipe(take(1)).subscribe((page) => {
       if (page > 1) {
-        this.store.dispatch(
-          MenuActions.changePage({ page: page - 1 })
-        );
+        this.store.dispatch(MenuActions.changePage({ page: page - 1 }));
       }
     });
-}
+  }
+  private orderStore: Store<OrderState> = inject(Store);
 
-  
+  addItem(quantity: number, plat: MenuItem) {
+    if (quantity < 1) return;
+    this.orderStore.dispatch(orderActions.addItem({ quantity, plat }));
+  }
 }
