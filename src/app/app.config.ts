fdbd4604ffefc053d45fc5bf2f7340f89351c10b
asma828 <asmalachhab66@gmail.com>
2026-01-21 15:54:43 +0100

    resolve conflicts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import * as orderEffects from './features/order/store/order.effects';
 
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { orderFeature } from './features/order/store/order.reducers';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { menuReducer } from './features/menu/store/menu.reducer';
 import { from } from 'rxjs';
 import {MenuEffects} from './features/menu/store/menu.effects';
+import { complaintReducer } from './features/complaint/store/complaint.reducer';
+import { ComplaintEffects } from './features/complaint/store/complaint.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideStore(),
-    provideState(orderFeature),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+     provideStore({
+          complaint: complaintReducer
+        }),
     provideState('menu', menuReducer),
-    provideEffects(orderEffects, MenuEffects),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-  ],
+    provideEffects(MenuEffects,ComplaintEffects),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
