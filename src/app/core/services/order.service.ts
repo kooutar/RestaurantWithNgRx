b88c49d38ca487a259a15b90f03b8f8da58a7ca5
Achraf Sikal <achrafsikal@gmail.com>
2026-01-21 12:01:00 +0100

    refactor: change order service implementation to use localStorage insted of in-memory mock data

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -1,59 +1,92 @@
 import { Injectable } from "@angular/core";
-import { delay, Observable, of } from "rxjs";
+import { Observable, of } from "rxjs";
 import { Order } from "../../features/order/order";
+import { LocalStorageService } from "./local-storage.service";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class OrderService {
-  private mockOrder: Order = {
-    orderId: '1',
-    items: [
-      {
-        platId: 'id1',
-        quantity: 2,
-      },
-      {
-        platId: 'id2',
-        quantity: 1,
-      },
-      {
-        platId: 'id3',
-        quantity: 3,
-      },
-    ],
-  };
-  getOrder(): Observable<Order> {
-    return of(this.mockOrder);
+  private order: Order;
+
+  constructor(private storageService: LocalStorageService<Order>) {
+    const storedOrder = this.storageService.get('order');
+    if (storedOrder) {
+      this.order = storedOrder;
+    } else {
+      this.order = {
+        orderId: Date.now().toString(),
+        items: [],
+      };
+      this.storageService.set('order', this.order);
+    }
   }
+
+  /**
+   * Retrieves the current order.
+   * @returns Observable of the current order
+   */
+  getOrder(): Observable<Order> {
+    return of(this.order);
+  }
+
+  /**
+   * Adds an item to the order with the specified platId and quantity.
+   * @param platId ID of the item to be added to the order
+   * @param quantity Quantity of the item to be added
+   * @returns Observable of updated Order after adding the item with the given platId and quantity
+   */
   addItem(platId: string, quantity: number): Observable<Order> {
     const updatedOrder: Order = {
-      ...this.mockOrder,
-      items: [...this.mockOrder.items, { platId, quantity }],
+      ...this.order,
+      items: [...this.order.items, { platId, quantity }],
     };
 
-    this.mockOrder = updatedOrder;
-
-    return of(updatedOrder);
+    return this.updateAndStoreOrder(updatedOrder);
   }
+
+  /**
+   * Removes an item from the order based on the provided platId.
+   * @param platId ID of the item to be removed from the order
+   * @returns Observable of updated Order after removing the item with the given platId
+   */
   removeItem(platId: string): Observable<Order> {
     const updatedOrder: Order = {
-      ...this.mockOrder,
-      items: this.mockOrder.items.filter((item) => item.platId !== platId),
+      ...this.order,
+      items: this.order.items.filter((item) => item.platId !== platId),
     };
 
-    this.mockOrder = updatedOrder;
-
-    return of(updatedOrder);
+    return this.updateAndStoreOrder(updatedOrder);
   }
+
+  /**
+   * Updates the quantity of a specific item in the order.
+   * @param platId ID of the item to be updated
+   * @param quantity New quantity for the item
+   * @returns Observable of updated Order after updating the quantity of the item with the given platId
+   */
   updateItemQuantity(platId: string, quantity: number): Observable<Order> {
     const updatedOrder: Order = {
-      ...this.mockOrder,
+      ...this.order,
       items: [
-        ...this.mockOrder.items.map((item) =>
-          item.platId === platId ? { platId, quantity } : item,
-        ),
+        ...this.order.items.map((item) => (item.platId === platId ? { platId, quantity } : item)),
       ],
     };
-    this.mockOrder = updatedOrder;
+    return this.updateAndStoreOrder(updatedOrder);
+  }
+
+  /**
+   * Clears all items from the current order.
+   * @returns Observable of updated Order after clearing all items
+   */
+  clearOrder(): Observable<Order> {
+    const updatedOrder: Order = {
+      ...this.order,
+      items: [],
+    };
+    return this.updateAndStoreOrder(updatedOrder);
+  }
+  private updateAndStoreOrder(updatedOrder: Order): Observable<Order> {
+    this.order = updatedOrder;
+    this.storageService.set('order', updatedOrder);
     return of(updatedOrder);
   }
 }
