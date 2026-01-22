2bee629cfbf67e37e92f1000fa726bc66e153a5d
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-01-22 12:00:00 +0100

    fix(store): register order reducer and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,20 +9,23 @@ import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { menuReducer } from './features/menu/store/menu.reducer';
 import { from } from 'rxjs';
-import {MenuEffects} from './features/menu/store/menu.effects';
+import * as orderEffects from './features/order/store/order.effects';
+import { MenuEffects } from './features/menu/store/menu.effects';
 import { complaintReducer } from './features/complaint/store/complaint.reducer';
 import { ComplaintEffects } from './features/complaint/store/complaint.effects';
+import { orderReducer } from './features/order/store/order.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-     provideStore({
-          complaint: complaintReducer
-        }),
+    provideStore({
+      complaint: complaintReducer,
+      order: orderReducer
+    }),
     provideState('menu', menuReducer),
-    provideEffects(MenuEffects,ComplaintEffects),
+    provideEffects(MenuEffects, ComplaintEffects, orderEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-]
+  ]
 };
